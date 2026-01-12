81d0bf6d874997de676ab17dd4dab2448532dde9
Max <maxi.gauthier112@gmail.com>
2026-01-12 13:05:04 +0100

    Ajout du modal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,31 @@
+import { use, useEffect, useState } from "react";
+
 export default function Home() {
+
+  // states
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch("https://api.github.com/users/github-johndoe");
+        if (!response.ok) {
+          throw new Error("Erreur réseau");
+        }
+        const data = await response.json();
+        setUser(data);
+      } catch (err) {
+        setError("Erreur de chargement des données");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <>
       <section className="hero justify-content-center d-flex align-items-center">
@@ -6,7 +33,8 @@ export default function Home() {
           <h1 className="fw-bold">John Doe</h1>
           <h2 className="h4 fw-semibold mb-4">Développeur Web</h2>
 
-          <button className="btn btn-primary">En savoir plus</button>
+          <button type="button" className="btn btn-primary" data-bs-toggle="modal"
+            data-bs-target="#githubModal">En savoir plus</button>
         </div>
       </section>
 
@@ -71,7 +99,56 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </section >
+      </section>
+
+      {/* GitHub Modal */}
+      <div className="modal fade" id="githubModal" tabIndex="-1" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+
+            <div className="modal-header">
+              <h5 className="modal-title">Profil GitHub</h5>
+              <button className="btn-close" data-bs-dismiss="modal" aria-label="Fermer"></button>
+            </div>
+
+            <div className="modal-body">
+              {loading && <p>Chargement...</p>}
+              {error && <p className="text-danger">{error}</p>}
+              {user && (
+                <div className="text-center">
+                  <img src={user.avatar_url} alt={user.login} className="img-fluid rounded-circle mb-3" width="100" />
+                  <h6>{user.name ?? user.login}</h6>
+                  {user.name && (
+                    <p className="text-muted mb-2">@{user.login}</p>
+                  )}
+                  {user.bio && <p className="mb-3">{user.bio}</p>}
+                  <a href={user.html_url} target="_blank" rel="nofollow noopener noreferrer" className="btn btn-primary">
+                    Voir le profil GitHub
+                  </a>
+                  <div className="my-4 d-flex justify-content-center gap-3 flex-wrap">
+                    <div className="border rounded px-3 py-2">
+                      Repos: {user.public_repos}
+                    </div>
+
+                    <div className="border rounded px-3 py-2">
+                      Followers: {user.followers}
+                    </div>
+
+                    <div className="border rounded px-3 py-2">
+                      Following: {user.following}
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+
+            <div className="modal-footer">
+              <button className="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </>
   );
 }
